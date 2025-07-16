05f191f3c99c9d6f208ff20b66cb26160579b6af
tanish-1 <imtanishlamba.com>
2025-07-16 18:37:01 +0530

    props topics explained

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import './App.css'
 import GoalList from "./components/GoalList";
 function App() {
+    const courseGoals = [
+        {id:'cg1',text:'Finish the Course'},
+        {id:'cg2',text:'Learn all about the Course Main Topic'},
+        {id:'cg3',text:'Help other students in the Course Q&amp;A'},]
   return (
       <div className="course-goals">
      <h2>Course Goals</h2>
-     <GoalList />
+     <GoalList goals={courseGoals} />
       </div>
    // <h1 title="This works!">Hi, this ReactJs</h1> // React.createElement('h1',{title: 'This works'},"Hi, this ReactJs!")
   )
