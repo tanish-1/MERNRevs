24b93c31ca2023ba482e4472112956efdccf0ee8
tanish-1 <imtanishlamba.com>
2025-07-16 18:18:10 +0530

    major changes applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import './App.css'
+import GoalList from "./components/GoalList";
 function App() {
   return (
-   <h1 title="This works!">Hi, this ReactJs</h1> // React.createElement('h1',{title: 'This works'},"Hi, this ReactJs!")
+      <div className="course-goals">
+     <h2>Course Goals</h2>
+     <GoalList />
+      </div>
+   // <h1 title="This works!">Hi, this ReactJs</h1> // React.createElement('h1',{title: 'This works'},"Hi, this ReactJs!")
   )
 }
 
