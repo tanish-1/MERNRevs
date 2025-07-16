d2080809198330677f9ed7fecf9e23105026f5b3
tanish-1 <imtanishlamba.com>
2025-07-16 18:52:33 +0530

    rendering props data with map function

--- src/components/GoalList.jsx
+++ src/components/GoalList.jsx
@@ -7,7 +7,9 @@ const GoalList= ({goals}) => {
     return (
 
         <ul className="goal-list">
-
+            { goals.map((goal,) => {
+                return <li key={goal.id}>{goal.text}</li>
+            })}
         </ul>
     );
 }
